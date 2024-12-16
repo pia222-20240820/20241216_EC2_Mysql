2af03879260db474429b9276e33fbd8d80f5e1fd
kyuyounglee <pia222.20240820@gmail.com>
2024-12-16 14:26:03 +0900

    pk 오류 수정

--- board/views.py
+++ board/views.py
@@ -5,7 +5,7 @@ def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'board/post_list.html',{'posts':posts})
 
-def post_detail(request):
+def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request, 'board/post_detail.html',{'post':post})
 
